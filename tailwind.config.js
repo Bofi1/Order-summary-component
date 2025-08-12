83547db886096cca62c67d29ef5e22d06da9e924
Bofi1 <angelborque03github@gmail.com>
2025-08-11 19:18:24 -0700

    mobile 1st part

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        redHat: ["Red Hat Display", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
